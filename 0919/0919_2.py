699e7014e7e72ec65444903b55d8fde403e04175
github Anggukk <goodhappy729@naver.com>
2025-09-19 13:59:49 +0900

    재귀함수 람다함수(map, filter, sort)

--- 0919/0919_2.py
+++ 0919/0919_2.py
@@ -86,3 +86,56 @@ for i in range(1, n+1):
 n = 4
 print(fibo)
 '''
+
+
+# 함다 함수(Lambda Function)
+'''
+람다 함수는 이름 없는 한 줄짜리 간단한 함수
+'''
+
+
+# 일반 함수
+def square(x):
+    return x**2
+
+
+# 람다 함수
+# square_lambda=lambda x : x**2
+
+
+print(square(5))
+# print(square_lambda(5))
+print((lambda x: x**2)(5))
+
+
+# map() : 모든 요소에 함수 적용
+numbers = [1, 2, 3, 4, 5]
+
+squared = list(map(lambda x: x**2, numbers))
+print(squared)
+
+
+def increment_fun(x):
+    return x+1
+
+
+incre_numbers = list(map(increment_fun, numbers))
+print(incre_numbers)
+
+
+# filter()
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+print(evens)
+
+
+# sorted() - 정렬 기준 지정
+
+students = [
+    {"name": "홍길동", "score": 80},
+    {"name": "김철수", "score": 92},
+    {"name": "이영희", "score": 72}
+]
+
+students.sort(key=lambda x: x["score"], reverse=True)
+for student in students:
+    print(f"{student["name"]} : {student["score"]}점")
