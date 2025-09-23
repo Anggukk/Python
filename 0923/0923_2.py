dbd236693acb0be678e132a4a3a20c6cfaef6372
github Anggukk <goodhappy729@naver.com>
2025-09-23 13:36:40 +0900

    모듈

---
+++ 0923/0923_2.py
@@ -0,0 +1,35 @@
+# 모듈
+'''
+파이썬 코드가 저장된 파일
+함수, 변수, 클래스 등을 모아놓은 파일로 다른 프로그램에서 가져다 쓸 수 있습니다.
+
+도구상자 : 여러 도구(함수)를 모아둔 상자(모듈)
+'''
+
+
+# 코드 재사용 : 한 번 작성한 코드를 여러 곳에서 사용
+# 유지 보수 : 기능별로 분리하여 관리가 쉬움
+# 협업 : 팀원들과 코드 공유가 편리
+# 네임스페이스 : 이름 충돌 방지
+
+
+# 전체 모듈 가져오기
+import calculator
+
+
+# 작성 되어 있는 모듈
+
+
+result = calculator.add(10, 5)
+print(result)
+
+
+# 실습1
+
+
+print(calculator.add(5, 6))
+print(calculator.subtract(10, 3))
+print(calculator.multiply(4, 5))
+print(calculator.divide(5, 4))
+
+print(calculator.divide(5, 0))
