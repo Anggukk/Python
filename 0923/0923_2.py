ca1a8bfdb59c13361025ae598632f67a18c0648d
github Anggukk <goodhappy729@naver.com>
2025-09-24 09:29:45 +0900

    모듈

--- 0923/0923_2.py
+++ 0923/0923_2.py
@@ -14,6 +14,8 @@
 
 
 # 전체 모듈 가져오기
+import random
+import math
 import calculator
 
 
@@ -54,3 +56,60 @@ print(calculator.divide(5, 0))
 
 # 가상 환경 비활성화
 # deactivate
+
+
+# 실습2
+# 문제1
+
+
+def dot(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return math.sqrt(math.pow((x2-x1), 2)+math.pow((y2-y1), 2))
+
+
+p1 = list(map(int, input("x1 y1 : ").split()))
+p2 = list(map(int, input("x2 y2 : ").split()))
+
+
+print(round(dot(p1, p2), 2))
+
+
+# 문제2
+print(f"최대공약수 : {math.gcd(18, 24)}")
+print(f"최소공배수 : {math.lcm(18, 24)}")
+
+
+# 실습3
+
+lotto = random.sample(range(1, 46), 6)
+lotto.sort()
+print(lotto)
+
+
+# 실습4
+user = input("가위/바위/보 : ")
+computer = random.choice(["가위", "바위", "보"])
+print("컴퓨터 : ", computer)
+
+if user == "가위":
+    if computer == "가위":
+        print("무")
+    elif computer == "바위":
+        pinrt("패")
+    else:
+        print("승")
+elif user == "바위":
+    if computer == "가위":
+        print("승")
+    elif computer == "바위":
+        pinrt("무")
+    else:
+        print("패")
+else:
+    if computer == "가위":
+        print("패")
+    elif computer == "바위":
+        pinrt("승")
+    else:
+        print("무")
